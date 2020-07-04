e582467cf08f9767cb979e32b131f9f2307cf934
Soumya Sharma <soum.sharma169@gmail.com>
2020-07-05 04:48:13 +0530

    Increase particles

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 160,
+        "value": 560,
         "density": {
           "enable": true,
           "value_area": 800
