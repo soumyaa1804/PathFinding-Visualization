f425729b8aef90e6a36c17e2d61c1b87ae9983ec
AnushaKittu <44207778+AnushaKittu@users.noreply.github.com>
2020-07-05 02:35:11 +0530

    add MouseEvents + upgraded Grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,173 @@
-/* Variables */
+// /* Variables */
+
+// var height = window.innerHeight * 0.8;
+// var width = window.innerWidth * 0.9;
+// var cellSize = 25;
+// var totalRows = Math.floor(height / cellSize) - 1;
+// var totalCols = Math.floor(width / cellSize) - 1;
+// var inProgress = false;
+// var cellsToAnimate = [];
+// var createWalls = false;
+// var algorithm = null;
+// var justFinished = false;
+// var animationSpeed = "Fast";
+// var animationState = null;
+// var startCell = [Math.floor(totalRows / 4), Math.floor(totalCols / 4)];
+// var endCell = [
+//   Math.floor((3 * totalRows) / 4),
+//   Math.floor((3 * totalCols) / 4),
+// ];
+// var movingStart = false;
+// var movingEnd = false;
+
+// /* Generate Grid */
+
+// function generateGrid(rows, cols) {
+//   var grid = `<table>`;
+//   for (row = 1; row <= rows; row++) {
+//     grid += `<tr>`;
+//     for (col = 1; col <= cols; col++) {
+//       let output = "cell";
+//       grid += `<td class = "${output}"></td>`;
+//     }
+//     grid += `</tr>`;
+//   }
+//   grid += `</table>`;
+//   return grid;
+// }
+
+// let grid = generateGrid(totalRows, totalCols);
+// document.getElementById("tableContainer").innerHTML = grid;
 
 var height = window.innerHeight * 0.8;
 var width = window.innerWidth * 0.9;
 var cellSize = 25;
 var totalRows = Math.floor(height / cellSize) - 1;
 var totalCols = Math.floor(width / cellSize) - 1;
-var inProgress = false;
-var cellsToAnimate = [];
-var createWalls = false;
-var algorithm = null;
-var justFinished = false;
-var animationSpeed = "Fast";
-var animationState = null;
-var startCell = [Math.floor(totalRows / 4), Math.floor(totalCols / 4)];
-var endCell = [
-  Math.floor((3 * totalRows) / 4),
-  Math.floor((3 * totalCols) / 4),
-];
-var movingStart = false;
-var movingEnd = false;
+// var inProgress = false;
+// var cellsToAnimate = [];
+// var createWalls = false;
+// var algorithm = null;
+// var justFinished = false;
+// var animationSpeed = "Fast";
+// var animationState = null;
+let keyDown = false;
+let mousePressed = false;
+let gridArray = [];
+let startRow = Math.floor(totalRows / 4);
+let startCol = Math.floor(totalCols / 4);
+let endRow = Math.floor((3 * totalRows) / 4);
+let endCol = Math.floor((3 * totalCols) / 4);
 
-/* Generate Grid */
-
-function generateGrid(rows, cols) {
-  var grid = "<table>";
-  for (row = 1; row <= rows; row++) {
-    grid += "<tr>";
-    for (col = 1; col <= cols; col++) {
-      grid += "<td></td>";
-    }
-    grid += "</tr>";
+//Instantiate the grid
+class Node {
+  constructor(row, col, nodeClass, nodeId) {
+    this.row = row;
+    this.col = col;
+    this.isStart = false;
+    this.isFinish = false;
+    this.isWall = false;
+    this.isVisited = false;
+    this.isClass = nodeClass;
+    this.id = nodeId;
+    this.status = nodeClass;
   }
-  grid += "</table>";
-  return grid;
 }
 
-var myGrid = generateGrid(totalRows, totalCols);
-$("#tableContainer").append(myGrid);
+//Generate the grid
+
+class Grid {
+  constructor() {
+    this.grid;
+  }
+  generateGrid(totalRows, totalCols) {
+    // grid += `<table>`;
+    for (let row = 0; row < totalRows; row++) {
+      let currRow = [];
+      grid += `<tr>`;
+      for (let col = 0; col < totalCols; col++) {
+        let new_nodeId = `${row}-${col}`,
+          new_nodeClass;
+        if (row === startRow && col === startCol) {
+          new_nodeClass = "start";
+        } else if (row === endRow && col === endCol) {
+          new_nodeClass = "end";
+        } else {
+          new_nodeClass = "unvisited";
+        }
+        //Instantiate a new Node object
+        let node = new Node(row, col, new_nodeClass, new_nodeId);
+        grid += `<td class = ${new_nodeClass} id = ${new_nodeId}></td>`;
+        currRow.push(node);
+      }
+      grid += `</tr>`;
+      gridArray.push(currRow);
+    }
+    grid = `<table>` + grid + `</table>`;
+    return grid;
+  }
+}
+let grid = new Grid();
+grid.generateGrid(totalRows, totalCols);
+document.getElementById("tableContainer").innerHTML = grid;
+//Create Walls
+/* 1) If the click is on the Start Node and it is being dragged then move the startNode
+   2) If the click is on the End Node and it is being dragged then change position
+   3) If the click is on a "unvisited" node then update "wall" and if dragged then createWalls
+   4) If the click is on a "visited" node then update and make it a unvisted node.*/
+//console.log(document.getElementById("1-1"));
+
+for (let r = 0; r < totalRows; r += 1) {
+  for (let c = 0; c < totalCols; c += 1) {
+    let currId = `${r}-${c}`;
+    //Current Node in the gridArray
+    let currNode = getNode(currId);
+    //Current Element in the grid
+    let currElement = document.getElementById(currId);
+
+    //  # Event Listeners --mousedown
+    //                      --mouseenter
+    //                      --mouseup
+    //             helper  --mousePressed
+
+    currElement.addEventListener("mousedown", () => {
+      mousePressed = true;
+      if (currNode.status === "start" || currNode.status === "end") {
+        pressedNodeStatus = currNode.status;
+      } else {
+        pressedNodeStatus = "normal";
+        //Manipulate the normal node - convert to "WALL" or "A normal node"
+        updateStatus(currNode);
+      }
+    });
+    currElement.addEventListener("mouseup", (e) => {
+      if (pressedNodeStatus === "start") {
+        start = currId;
+      } else if (board.pressedNodeStatus === "end") {
+        object = currId;
+      }
+      pressedNodeStatus = "normal";
+    });
+    currElement.addEventListener("mouseenter", (e) => {});
+  }
+}
+function getNode(id) {
+  let info = id.split("-");
+  let row = parseInt(info[0]);
+  let col = parseInt(info[1]);
+  return gridArray[row][col];
+}
+function updateStatus(currNode) {
+  let element = document.getElementById(currNode.id);
+  console.log(element, currNode);
+  relevantStatuses = ["start", "end"];
+  if (!keyDown) {
+    if (!relevantStatuses.includes(currNode.status)) {
+      element.className = currNode.status !== "wall" ? "wall" : "unvisited";
+      currNode.status = element.className !== "wall" ? "unvisited" : "wall";
+    }
+  }
+}
 
 /* ---------------------- */
 /*-- Draggable Feature -- */
@@ -150,27 +281,32 @@ class MinHeap {
 /* ---- MOUSE FUNCTIONS ---- */
 /* ------------------------- */
 
-$("td").mousedown(
-  () => {
-    var index = $("td").index(this);
-    var startCellIndex = (startCell[0] * (totalCols)) + startCell[1];
-    var endCellIndex = (endCell[0] * (totalCols)) + endCell[1];
-    if (!inProgress) {
-      // Clear grid if just finished and keep walls
-      if (justFinished && !inProgress) {
-        clearBoard(keepWalls = true);
-        justFinished = false;
-      }
-      if (index == startCellIndex) {
-        movingStart = true;
-        console.log(`Now moving start! ${index}`);
-      } else if (index == endCellIndex) {
-        movingEnd = true;
-        console.log(`Moving ends ${index}`);
-      } else {
-        createWalls = true;
-        console.log("Walls in making!" + index)
-      }
-    }
-  }
-);
+//Getting only one cell that is cell[0][0]
+// document
+//   .querySelector("#tableContainer")
+//   .addEventListener("mousedown", HandleMousedown);
+// function HandleMousedown(e) {
+//   console.log("mousedown");
+//   e.preventDefault();
+// }
+// $("td").mousedown(function () {
+//   var index = $("td").index(this);
+//   var startCellIndex = startCell[0] * totalCols + startCell[1];
+//   var endCellIndex = endCell[0] * totalCols + endCell[1];
+//   if (!inProgress) {
+//     // Clear board if just finished
+//     if (justFinished && !inProgress) {
+//       clearBoard((keepWalls = true));
+//       justFinished = false;
+//     }
+//     if (index == startCellIndex) {
+//       movingStart = true;
+//       //console.log("Now moving start!");
+//     } else if (index == endCellIndex) {
+//       movingEnd = true;
+//       //console.log("Now moving end!");
+//     } else {
+//       createWalls = true;
+//     }
+//   }
+// });
