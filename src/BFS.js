2e010d8b4699f852e65c26672d8689f8393cf464
Soumya Sharma <soum.sharma169@gmail.com>
2020-07-18 20:39:35 +0530

    Check for diagonal walls

--- src/BFS.js
+++ src/BFS.js
@@ -13,7 +13,7 @@ export function BFS(nodesToAnimate, pathFound) {
   console.log("Grid node", gridArray[startNode.row][startNode.col]);
   myQueue.enqueue(startNode);
   nodesToAnimate.push([startNode, "searching"]);
-  var currNode = new Node();
+  let currNode = new Node();
   while (!myQueue.empty()) {
     currNode = myQueue.dequeue();
     var r = currNode.row;
@@ -30,6 +30,8 @@ export function BFS(nodesToAnimate, pathFound) {
       var n = neighbours[k][1];
       let node = new Node();
       node = gridArray[m][n];
+      gridArray[m][n].isVisited = true;
+      gridArray[m][n].parent = currNode;
       nodesToAnimate.push([node, "searching"]);
       myQueue.enqueue(node);
     }
@@ -51,6 +53,8 @@ export function BFS(nodesToAnimate, pathFound) {
 function getNeighbours(i, j) {
   var neighbors = [];
   // direction vectors
+  // 0-3: East, South, West, North
+  // 4-7: South-East, North-East, South-West, North-West
   const dx = [1, 0, -1, 0, 1, 1, -1, -1];
   const dy = [0, 1, 0, -1, 1, -1, 1, -1];
 
@@ -58,11 +62,22 @@ function getNeighbours(i, j) {
     let rr = i+dx[d];
     let cc = j+dy[d];
     if((rr >= 0) && (rr < totalRows) && (cc >= 0) && (cc < totalCols)) {
-      if(gridArray[rr][cc].isVisited || gridArray[rr][cc].status == "wall"){
+      if(gridArray[rr][cc].isVisited || gridArray[rr][cc].status === "wall"){
         continue;
-      } else {
-        gridArray[rr][cc].isVisited = true;
-        gridArray[rr][cc].parent = gridArray[i][j];
+      } // if d < 4, push elements else if d >= 4, check for diagonal walls  
+      else if(d < 4){
+        neighbors.push([rr, cc]);
+      } 
+      else if(d === 4 && (gridArray[i][j+1].status !== "wall") && (gridArray[i+1][j].status !== "wall")) {
+        neighbors.push([rr, cc]);
+      } 
+      else if(d === 5 && (gridArray[i][j-1].status !== "wall") && (gridArray[i+1][j].status !== "wall")) {
+        neighbors.push([rr, cc]);
+      } 
+      else if(d === 6 && (gridArray[i-1][j].status !== "wall") && (gridArray[i][j+1].status !== "wall")) {
+        neighbors.push([rr, cc]);
+      } 
+      else if(d === 7 && (gridArray[i-1][j].status !== "wall") && (gridArray[i][j-1].status !== "wall")) {
         neighbors.push([rr, cc]);
       }
     }
